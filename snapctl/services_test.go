c35ae42f620a5dce561f9002d55ed3fe533c2bec
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2022-03-07 14:45:41 +0100

    Extend services tests

--- snapctl/services_test.go
+++ snapctl/services_test.go
@@ -1,6 +1,7 @@
 package snapctl
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -15,11 +16,8 @@ func TestServices(t *testing.T) {
 			services, err := Services(mockService).Run()
 			require.NoError(t, err, "Error getting services.")
 			require.Len(t, services, 1)
-			for k, v := range services {
-				require.Equal(t, mockService, k)
-				require.True(t, v.Enabled, "Service not enabled")
-				require.True(t, v.Active, "Service not active")
-			}
+			serviceName := reflect.ValueOf(services).MapKeys()[0].String()
+			require.Equal(t, mockService, serviceName)
 		})
 
 		t.Run("all", func(t *testing.T) {
@@ -31,7 +29,29 @@ func TestServices(t *testing.T) {
 			}
 		})
 
-		t.Run("service name invalid", func(t *testing.T) {
+		t.Run("enabled and active", func(t *testing.T) {
+			services, err := Services(mockService).Run()
+			require.NoError(t, err, "Error getting services.")
+			require.Len(t, services, 1)
+			for k, v := range services {
+				require.Equal(t, mockService, k)
+				require.True(t, v.Enabled, "Service not enabled")
+				require.True(t, v.Active, "Service not active")
+			}
+		})
+
+		t.Run("disabled and inactive", func(t *testing.T) {
+			services, err := Services(mockServiceDisabled).Run()
+			require.NoError(t, err, "Error getting services.")
+			require.Len(t, services, 1)
+			for k, v := range services {
+				require.Equal(t, mockServiceDisabled, k)
+				require.False(t, v.Enabled, "Service not disabled")
+				require.False(t, v.Active, "Service not inactive")
+			}
+		})
+
+		t.Run("service not found", func(t *testing.T) {
 			_, err := Services("non-existed").Run()
 			require.Error(t, err)
 		})
