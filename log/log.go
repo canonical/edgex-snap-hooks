2db75eb57826975a4fe23e6c2460a2a5132571c0
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2022-03-08 13:22:46 +0100

    Add suffix and debug control

--- log/log.go
+++ log/log.go
@@ -20,12 +20,47 @@ import (
 	"fmt"
 	"log/syslog"
 	"os"
+
+	"github.com/canonical/edgex-snap-hooks/v2/snapctl"
 )
 
 var (
-	slog  *syslog.Writer
-	debug bool = false
+	slog            *syslog.Writer
+	debug           bool
+	snapInstanceKey string // used as default syslog tag and tag prefix
 )
+
+func init() {
+	if value, err := snapctl.Get("debug").Run(); err != nil {
+		panic(err)
+	} else {
+		debug = (value == "true")
+	}
+
+	snapInstanceKey = os.Getenv("SNAP_INSTANCE_NAME")
+	if snapInstanceKey == "" {
+		panic("SNAP_INSTANCE_NAME environment variable not set.")
+	}
+
+	var err error
+	slog, err = syslog.New(syslog.LOG_INFO, snapInstanceKey)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// SetComponentName adds a component name to syslog app name as "my-snap.<component>"
+// The default app name is just "my-snap", read from the snap environment.
+// This function is NOT thread-safe. It should not be called concurrently with
+// the other logging functions of this package.
+// Syslog app name: https://datatracker.ietf.org/doc/html/rfc5424#section-6.2.5
+func SetComponentName(component string) {
+	var err error
+	slog, err = syslog.New(syslog.LOG_INFO, snapInstanceKey+"."+component)
+	if err != nil {
+		panic(err)
+	}
+}
 
 // Debug writes the given input to syslog (sev=LOG_DEBUG) if snap `debug`
 // configuration option is set to `true`.
@@ -80,21 +115,4 @@ func Warn(a ...interface{}) {
 // It formats similar to fmt.Sprintf
 func Warnf(format string, a ...interface{}) {
 	Warn(fmt.Sprintf(format, a...))
-}
-
-func init() {
-	// TODO: use snapctl get debug to read this
-	// Depends on https://github.com/canonical/edgex-snap-hooks/pull/26
-	debug = true
-
-	snap := os.Getenv("SNAP")
-	if snap == "" {
-		panic("SNAP environment variable is not set")
-	}
-
-	var err error
-	slog, err = syslog.New(syslog.LOG_INFO, snap+":hook")
-	if err != nil {
-		panic(err)
-	}
 }
