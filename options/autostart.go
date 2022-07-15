9f3fb6ab0254f90a4efcd85981050c92dc161515
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2022-07-15 15:09:46 +0200

    Switch to bool pointer to distinguish unset option

--- options/autostart.go
+++ options/autostart.go
@@ -9,7 +9,7 @@ import (
 	"github.com/canonical/edgex-snap-hooks/v2/snapctl"
 )
 
-func processAppAutostartOptions(apps []string) (map[string]bool, error) {
+func processAppAutostartOptions(apps []string) (map[string]*bool, error) {
 	// get the apps' json structure
 	jsonString, err := snapctl.Get("apps").Document().Run()
 	if err != nil {
@@ -21,50 +21,60 @@ func processAppAutostartOptions(apps []string) (map[string]bool, error) {
 		return nil, fmt.Errorf("error unmarshalling 'apps' option: %s", err)
 	}
 
-	appAutostart := make(map[string]bool)
+	appAutostart := make(map[string]*bool)
 	for _, app := range apps {
 		// get the configuration specified for each app
-		appOptions := options.Apps[app]
-		if appOptions.Autostart == nil {
+		autostart := options.Apps[app].Autostart
+		if autostart == nil {
 			// no autostart option for the app
 			continue
 		}
 
-		appAutostart[app], err = parseAutostart(*appOptions.Autostart)
+		bValue, err := parseAutostart(*autostart)
 		if err != nil {
 			return nil, fmt.Errorf("error parsing autostart for %s: %v", app, err)
+		}
+		if bValue != nil {
+			appAutostart[app] = *&bValue
 		}
 	}
 
 	return appAutostart, nil
 }
 
-func processGlobalAutostartOptions(apps []string) (map[string]bool, error) {
+func processGlobalAutostartOptions(apps []string) (map[string]*bool, error) {
 	autostart, err := snapctl.Get("autostart").Run()
 	if err != nil {
 		return nil, fmt.Errorf("error reading 'autostart' option: %s", err)
 	}
 
-	appAutostart := make(map[string]bool)
+	appAutostart := make(map[string]*bool)
 	for _, app := range apps {
-		appAutostart[app], err = parseAutostart(autostart)
+		bValue, err := parseAutostart(autostart)
 		if err != nil {
 			return nil, fmt.Errorf("error parsing autostart for %s: %v", app, err)
+		}
+		if bValue != nil {
+			appAutostart[app] = *&bValue
 		}
 	}
 
 	return appAutostart, nil
 }
 
-func parseAutostart(value string) (bool, error) {
+func parseAutostart(value string) (*bool, error) {
 	value = strings.ToLower(value)
 	switch value {
+	case "":
+		return nil, nil
 	case "true", "yes":
-		return true, nil
+		b := true
+		return &b, nil
 	case "false", "no":
-		return false, nil
+		b := false
+		return &b, nil
 	default:
-		return false, fmt.Errorf("invalid value for 'autostart': '%s'", value)
+		return nil, fmt.Errorf("invalid value for 'autostart': '%s'", value)
 	}
 }
 
@@ -88,7 +98,7 @@ func ProcessAutoStart(apps ...string) error {
 			autostart = a
 		}
 		log.Debugf("app %s: autostart=%t", app, appAutostart[app])
-		if autostart {
+		if autostart != nil && *autostart {
 			err = snapctl.Start(app).Enable().Run()
 			if err != nil {
 				return fmt.Errorf("error starting service: %s", err)
